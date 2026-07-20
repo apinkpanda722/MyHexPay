23f44ceb018e9645aed404a4dc3e57f971e80a53
SpanLee-Time <iih907@mediazen.co.kr>
2026-07-20 17:03:51 +0900

    feat: 멤버십 도메인 모델 및 등록 어댑터 구현
    - domain 패키지에 Membership 도메인 모델과 Value Object(MembershipId, MembershipName, MembershipEmail, MembershipAddress, MembershipIsValid, MembershipIsCorp) 추가
    - TestApplication을 MembershipApplication으로 이름 정리
    - TestController를 adapter/in/web/RegisterMembershipController로 이동하여 헥사고날 인바운드 어댑터 위치 정리

--- membership-service/src/main/java/com/hexpay/membership/adapter/in/web/RegisterMembershipController.java
+++ membership-service/src/main/java/com/hexpay/membership/adapter/in/web/RegisterMembershipController.java
@@ -1,4 +1,4 @@
-package com.hexpay.membership;
+package com.hexpay.membership.adapter.in.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-public class TestController {
+public class RegisterMembershipController {
 
     @GetMapping(path = "/test")
     void test() {
