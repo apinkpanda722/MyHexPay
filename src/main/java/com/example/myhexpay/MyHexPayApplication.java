70c2aacf961ca6efbc912f7e284de4f5b0c37d46
SpanLee-Time <iih907@mediazen.co.kr>
2026-07-20 11:09:03 +0900

    first commit

---
+++ src/main/java/com/example/myhexpay/MyHexPayApplication.java
@@ -0,0 +1,13 @@
+package com.example.myhexpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHexPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyHexPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myhexpay/MyHexPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myhexpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHexPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
